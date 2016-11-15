2a917dbed69962e499c80da39d0d7cc80c3651f0
hridayam <hridayambakshi2011@gmail.com>
2016-11-14 18:27:58 -0800

    added price

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
 	has_many :comments
 	validates :title, presence: true, length: {maximum: 50}
 	validates :post, presence: true
+	validates :price, presence: true, numericality: {greater_than_or_equal_to: 0, only_integer: true}
 end
--- app/views/posts/_post.json.jbuilder
+++ app/views/posts/_post.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! post, :id, :title, :post, :address, :user_id, :created_at, :updated_at
+json.extract! post, :id, :title, :post, :address, :user_id, :created_at, :updated_at, :price
 json.url post_url(post, format: :json)
