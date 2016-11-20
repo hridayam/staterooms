5eb2de63820ac066e400b033d158b405837399a5
Gregory Gan <gregory.t.gan@gmail.com>
2016-11-19 20:13:08 -0800

    Latest working version; making sure it keeps existing. Added a password confirmation and set input to password field.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments
-	validates :title, presence: true, length: {maximum: 50}
+	validates :title, presence: true, length: { maximum: 50 }
 	validates :post, presence: true
-	validates :price, presence: true, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+	validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
 	has_many :posts
 	has_many :comments
-	validates :password, presence: true,length: { minimum: 8 }
-	validates :email, presence: true
+
+	validates :email, presence: true, format: { with: /\A\w+.\w+@sjsu.edu\z/, message: "isn't an SJSU email" }, uniqueness: true
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+	validates :password, presence: true, length: { minimum: 8 }, confirmation: true
+    validates :password_confirmation, presence: true, length: { minimum: 8 }
 end
