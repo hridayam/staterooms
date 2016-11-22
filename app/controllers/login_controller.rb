b3023ed26106b3e87aa89a1df0eb059295aaa21d
hridayam <hridayambakshi2011@gmail.com>
2016-11-22 01:24:00 -0800

    added email validity check

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,3 +1,8 @@
 class LoginController < ApplicationController
 
+	def login
+		@users = User.all
+		
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
 	has_many :posts
 	has_many :comments
 	validates :password, presence: true,length: { minimum: 8 }
-	validates :email, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@sjsu.edu+\z/i
+	validates :email, presence: true, uniqueness: {case_sensitive: false},
+						format: {with: VALID_EMAIL_REGEX}
+
 end
