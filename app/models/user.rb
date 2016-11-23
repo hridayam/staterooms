12ba7fa526ce5e2b1022059887d2f0296682d33c
hridayam <hridayambakshi2011@gmail.com>
2016-11-22 16:39:24 -0800

    create user with secure password and show user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+	has_many :posts
+	has_many :comments
+	validates :password, presence: true,length: { minimum: 8 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@sjsu.edu+\z/i
+	validates :email, presence: true, uniqueness: {case_sensitive: false},
+						format: {with: VALID_EMAIL_REGEX}
+	has_secure_password
+end
