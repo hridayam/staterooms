242d5d79fbc9e563cab1dd7baf226da44ab8c48e
Gregory Gan <gregory.t.gan@gmail.com>
2016-11-29 06:32:04 -0800

    Fixed stupid mistake in search query

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,8 +6,7 @@ class Listing < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def self.search(search)
-    where("title LIKE ?", "%#{search}%")
-    where("description LIKE ?", "%#{search}%")
-    where("address LIKE ?", "%#{search}%")
+    where("title LIKE ? OR description LIKE ? OR address LIKE ?",
+          "%#{search}%", "%#{search}%", "%#{search}%")
   end
 end
